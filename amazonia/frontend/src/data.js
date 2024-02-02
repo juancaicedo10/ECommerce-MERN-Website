97d00e08bdcd878bfa63bf1de5de7b2824a1a5df
Juan Esteban Caicedo Valencia <imjuan3772@gmail.com>
2024-02-02 12:06:54 -0500

    new functionalities

---
+++ amazonia/frontend/src/data.js
@@ -0,0 +1,57 @@
+import nikeShirt from './images/p1.jpg';
+import adidasShirt from './images/p2.jpg';
+import adidasPants from './images/p3.jpg';
+import adidasFitPants from './images/p4.jpg'
+
+export const data = {
+    products: [
+        {
+            name: 'Nike Slim shirt',
+            slug: 'nike-slim-shirt',
+            category: 'Shirts',
+            image: nikeShirt,
+            price: 120,
+            countInStock: 10,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'Hight quality shirt'
+        },
+        {
+            name: 'Adidas Slim shirt',
+            slug: 'adidas-slim-shirt',
+            category: 'Shirts',
+            image: adidasShirt,
+            price: 120,
+            countInStock: 10,
+            brand: 'Adidas',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'Hight quality product'
+        },
+        {
+            name: 'Nike Slim Pant',
+            slug: 'nike-slim-pantt',
+            categorgy: 'Pants',
+            image: adidasPants,
+            price: 120,
+            countInStock: 10,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'Hight quality pant'
+        },
+        {
+            name: 'Adidas Slim shirt',
+            slug: 'adidas-fit-shirt',
+            category: 'Shirts',
+            image: adidasFitPants,
+            price: 120,
+            countInStock: 10,
+            brand: 'Adidas',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'Hight quality product'
+        },
+    ]
+}
